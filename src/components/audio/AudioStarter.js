4b862955463da5a0093c1074f060395f31f3614b
Daniel Miller <dbmiller25@gmail.com>
2021-02-11 17:16:49 -0500

    Some more pause menu things

--- src/components/audio/AudioStarter.js
+++ src/components/audio/AudioStarter.js
@@ -8,7 +8,7 @@ const AudioStarter = ({ song, positions1 }) => {
   const sound = useRef();
 
   const pauseMenu = (e) => {
-    console.log(sound.current.isPlaying);
+    // console.log(sound.current.isPlaying);
     if (e.key === " " && sound.current.isPlaying) {
       sound.current.pause();
       return;
@@ -21,31 +21,34 @@ const AudioStarter = ({ song, positions1 }) => {
     document.addEventListener("keydown", (e) => pauseMenu(e));
   });
   return (
-    <Suspense fallback={null}>
-      <PositionalAudio
-        url={song}
-        ref={sound}
-        distance={10000}
-        position={[250, 0, 0]}
-      />
-      {positions1.map((position) => {
-        let scale = Math.floor(Math.random() * 9) + 3;
-        let scaleX = (Math.random() * 1 + 0.5) * scale;
-        let scaleY = (Math.random() * 1 + 0.5) * scale;
-        const yRotate = Math.random() * Math.PI;
-        const mushroomType = Math.floor(Math.random() * 100) < 88 ? 1 : 2;
-        return (
-          <Mushroom
-            sound={sound}
-            scale={[scaleX, scaleY, scaleX]}
-            color={randomColor()}
-            position={position}
-            yRotate={yRotate}
-            mushroomType={mushroomType}
-          />
-        );
-      })}
-    </Suspense>
+    <>
+      
+      <Suspense fallback={null}>
+        <PositionalAudio
+          url={song}
+          ref={sound}
+          distance={10000}
+          position={[250, 0, 0]}
+        />
+        {positions1.map((position) => {
+          let scale = Math.floor(Math.random() * 9) + 3;
+          let scaleX = (Math.random() * 1 + 0.5) * scale;
+          let scaleY = (Math.random() * 1 + 0.5) * scale;
+          const yRotate = Math.random() * Math.PI;
+          const mushroomType = Math.floor(Math.random() * 100) < 88 ? 1 : 2;
+          return (
+            <Mushroom
+              sound={sound}
+              scale={[scaleX, scaleY, scaleX]}
+              color={randomColor()}
+              position={position}
+              yRotate={yRotate}
+              mushroomType={mushroomType}
+            />
+          );
+        })}
+      </Suspense>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function Root() {
   return (
     <>
       <App />
-      
     </>
   );
 }
