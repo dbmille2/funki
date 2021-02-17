3824b4a116632bfc2d0368dc4bd25d525da4aa5e
Daniel Miller <dbmiller25@gmail.com>
2021-02-17 17:04:16 -0500

    Links in navbar:

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -19,8 +19,21 @@ const NavBar = () => {
         <span className="app-name">funki</span>
       </div>
       <div className="pause-navbar-right">
-        <AiOutlineGithub className="github-button" />
-        <AiOutlineLinkedin className="linkedin-button" />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/dbmille2/funki"
+        >
+          <AiOutlineGithub className="github-button" />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/daniel-miller-970393178/"
+        >
+          <AiOutlineLinkedin className="linkedin-button" />
+        </a>
+
         <AiOutlineInfoCircle className="info-button" />
       </div>
     </div>
